63bce5741dede474560ed94f0a943932634e3d4c
Ry2X <45420571+ry2x@users.noreply.github.com>
2023-05-11 01:54:13 +0900

    Delete trash

--- src/subCommands/subcommandtest/grouptest/pingping.ts
+++
@@ -1,8 +0,0 @@
-import type { ChatInputCommandInteraction } from 'discord.js';
-import SubCommand from '../../../templates/SubCommand.js';
-
-export default new SubCommand({
-  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
-    await interaction.reply('PongPong!');
-  },
-});
